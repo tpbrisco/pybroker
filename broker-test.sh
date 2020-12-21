879816683ef2aa1c16a361563fdd76dddacd284f
tpbrisco <tpbrisco+gh@gmail.com>
2020-12-21 23:09:07 +0000

    better error output, better flags

--- broker-test.sh
+++ broker-test.sh
@@ -2,7 +2,7 @@
 set -eou pipefail
 
 # CURL_FLAGS -- e.g. put "-k" in your environment if your certificates aren't right
-CFLAGS=${CURL_FLAGS:-''}
+CURL_FLAGS=${CURL_FLAGS:-''}
 
 # set broker URL - default to cfdev environment
 URL=${BROKER_URL:-"http://broker.dev.cfdev.sh"}
@@ -17,16 +17,15 @@ fi
 
 # fetch the catalog that the market place would bind to
 echo Discovering service and plans
-curl ${CFLAGS} -s -u user:pass -H "X-Broker-API-Version: 2.10" \
+curl ${CURL_FLAGS} -sS -u user:pass -H "X-Broker-API-Version: 2.10" \
      ${URL}/v2/catalog | \
     jq '.services[] | {service_id: .id, plan_id: .plans[].id}'
 
-
 # get a plan id for provisioning it
-PLAN_ID=$(curl ${CFLAGS} -s -u user:pass -H "X-Broker-API-Version: 2.10" \
+PLAN_ID=$(curl ${CURL_FLAGS} -sS -u user:pass -H "X-Broker-API-Version: 2.10" \
 	       ${URL}/v2/catalog | \
 	      jq '.services[0].plans[0].id')
-SVC_ID=$(curl ${CFLAGS} -s -u user:pass -H "X-Broker-API-Version: 2.10" \
+SVC_ID=$(curl ${CURL_FLAGS} -sS -u user:pass -H "X-Broker-API-Version: 2.10" \
 	      ${URL}/v2/catalog | \
 	     jq '.services[0].id')
 
@@ -34,7 +33,7 @@ SVC_ID=$(curl ${CFLAGS} -s -u user:pass -H "X-Broker-API-Version: 2.10" \
 echo Create service instance
 echo "{\"service_id\": ${SVC_ID}, \"plan_id\": ${PLAN_ID}}" > /tmp/broker_data
 INSTANCE_ID=$(uuidgen)
-curl ${CFLAGS} -s -u user:pass -X PUT \
+curl ${CURL_FLAGS} -sS -u user:pass -X PUT \
      -H 'X-Broker-API-Version: 2.12' \
      -H "Content-Type: application/json" \
      -d @/tmp/broker_data \
@@ -43,7 +42,7 @@ curl ${CFLAGS} -s -u user:pass -X PUT \
 rm -f /tmp/broker_data
 
 # verify that it is there
-FETCH_ID=$(curl ${CFLAGS} -s -u user:pass -X GET -H 'X-Broker-API-Version: 2.12' \
+FETCH_ID=$(curl ${CURL_FLAGS} -sS -u user:pass -X GET -H 'X-Broker-API-Version: 2.12' \
 		${URL}/console | \
 	       jq ".instances[\"${INSTANCE_ID}\"].id")
 if [[ $FETCH_ID != \"$INSTANCE_ID\" ]];
@@ -57,7 +56,7 @@ fi
 # bind to a app (the broker itself)
 BIND_ID=$(uuidgen)
 echo "{\"service_id\": ${SVC_ID}, \"plan_id\": ${PLAN_ID}}" > /tmp/bind_data
-curl ${CFLAGS} -s -u user:pass -X PUT \
+curl ${CURL_FLAGS} -sS -u user:pass -X PUT \
      -H 'X-Broker-API-Version: 2.12' \
      -H "Content-Type: application/json" \
      -d @/tmp/bind_data \
@@ -67,7 +66,7 @@ rm -f /tmp/bind_data
 echo "Bound (${BIND_ID}) to instance ${INSTANCE_ID}"
 
 # delete the created ID
-curl ${CFLAGS} -s -u user:pass -X DELETE \
+curl ${CURL_FLAGS} -sS -u user:pass -X DELETE \
      -H 'X-Broker-API-Version: 2.12' \
      -o /dev/null \
      ${URL}/v2/service_instances/${INSTANCE_ID}/service_bindings/${BIND_ID}
@@ -78,11 +77,11 @@ echo Deleted service instance $INSTANCE_ID
 # clean up all instances
 #
 echo Cleaning up all bindings
-for guid in $(curl ${CFLAGS} -s -X GET ${URL}/console | jq '.instances[].id');
+for guid in $(curl ${CURL_FLAGS} -sS -X GET ${URL}/console | jq '.instances[].id');
 do
     echo "  $guid"
     foo=$(echo $guid | sed -e's/\"//g');
-    curl ${CFLAGS} -s -u user:pass -X DELETE \
+    curl ${CURL_FLAGS} -sS -u user:pass -X DELETE \
 	 -o /dev/null \
 	 ${URL}/v2/service_instances/$foo;
 done
