7b84ddfa8fe634cfe88fbe4b85740dfec6bb4a0a
tpbrisco <4596011+tpbrisco@users.noreply.github.com>
2022-11-24 06:55:39 -0500

    dont default to URL

--- expose-broker.sh
+++ expose-broker.sh
@@ -1,4 +1,9 @@
 set -e
+
+if [[ -z "BROKER_URL" ]]; then
+    echo "BROKER_URL should be set to the ... url"
+    exit 1
+fi
 # set broker URL if defined
 URL=${BROKER_URL:-"http://broker.dev.cfdev.sh"}
 echo "Using broker URL ${URL}"
