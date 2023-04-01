307a2ca27ba4c4170e8ccfff45b5fac8e90b02bb
tpbrisco <4596011+tpbrisco@users.noreply.github.com>
2023-04-01 08:12:05 -0400

    tidy deployment scripts

--- expose-broker.sh
+++ expose-broker.sh
@@ -4,10 +4,9 @@ if [[ -z "BROKER_URL" ]]; then
     echo "BROKER_URL should be set to the ... url"
     exit 1
 fi
-# set broker URL if defined
-URL=${BROKER_URL:-"http://broker.dev.cfdev.sh"}
-echo "Using broker URL ${URL}"
+
+echo "Using broker URL ${BROKER_URL}"
 
 # use "--space-scoped" if not admin
-cf create-service-broker dream user pass ${URL}
+cf create-service-broker dream user pass ${BROKER_URL}
 cf enable-service-access dream
--- hide-broker.sh
+++ hide-broker.sh
@@ -1,8 +1,11 @@
 set -e
 
-# set broker URL - default to cfdev environment
-URL=${BROKER_URL:-"http://broker.dev.cfdev.sh"}
-echo "Using broker URL ${URL}"
+if [[ -z "BROKER_URL" ]]; then
+    echo "BROKER_URL should be set to the ... url"
+    exit 1
+fi
 
+# ignore errors -- forge through the decommissioning
+set +e
 cf disable-service-access dream
 cf delete-service-broker dream -f
